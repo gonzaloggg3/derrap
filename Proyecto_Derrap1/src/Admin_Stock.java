49f1f028eb317c884e38555618f01269ab7ef9d4
kevinballes <kballes20@gmail.com>
2025-02-06 14:12:48 +0100

    admin

--- Proyecto_Derrap1/src/Admin_Stock.java
+++ Proyecto_Derrap1/src/Admin_Stock.java
@@ -6,7 +6,7 @@ import javax.swing.border.EmptyBorder;
 import java.sql.*;
 
 public class Admin_Stock extends JFrame {
-
+//hhfdhfoehfhs
     private static final long serialVersionUID = 1L;
     private JPanel contentPane;
     private JTextField txtDNI;
